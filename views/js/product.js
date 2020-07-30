8eb66108c0ce659f699f29f6ca400dce8503d746
Gladston Aristoverne <gladston.aristoverne.ga@gmail.com>
2020-07-30 18:31:35 -0400

    display product in product page

--- views/js/product.js
+++ views/js/product.js
@@ -3,7 +3,7 @@ When user click on personalize link, we display product
 with id on the product page
 -------------------------------------------------------*/
 const url = "http://localhost:3000/api/cameras";
-const products = document.querySelector('.personalizeJs'); // page produit
+const productsDOM = document.querySelector('.personalizeJs'); // page produit
 
 
 
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
     getOneProduct.getOneCameraInProductPage();
 });
 
-export class Personalize {
+class Personalize {
     async getOneCameraInProductPage() {
         let params = new URLSearchParams(window.location.search);
         params.has('id');
@@ -22,7 +22,7 @@ export class Personalize {
         let res = await fetch(url+"/"+id);
         let product = await res.json()
             .then((product) => {
-                     products.innerHTML = ` <article class="show-product">
+                     productsDOM.innerHTML = ` <article class="show-product">
                      <div class="products-img">
                          <img src="${product.imageUrl}" alt="image-${product.name}" class="img-camera">
                      </div>
@@ -30,62 +30,16 @@ export class Personalize {
                          <h2> ${product.name}</h2>
                          <p> ${product.description}</p>
                          <h3> ${product.price} €</h3>
-                         <button type="button" id="${product.id}" class="btn btn-add-cart" data-id=${product.id}> Ajouter
+                         <button type="button" id="${product.id}" class="btn add-to-cart" data-id=${product.id}> Ajouter
                              au panier</button>
                      </div>
                  </article>`;
                  })
             .catch(error => { console.log(error) })
-
     }
 }
 
 
 
- /*---------------------------------------------------
-                ADD TO CART - LOCAL STORAGE
------------------------------------------------------*/
-let cart = [];
-let buttonsDOM = [];
-
-function addToCart() {
-    let btnAddToCart = document.querySelectorAll('.btn-add-cart');
-    buttonsDOM = btnAddToCart;
-
-
-        if (inCart) {
-            button.innerText = "Ajouté au panier";
-            button.disabled = true;
-        }
-
-        button.addEventListener('click', () => {
-            console.log('Ajouté au panier')
-        })
-    // le produit est ajouter au panier, on alert "Ajouté au panier"
-
-    // produit est gardé dans la session storage
-    
-    // l'utilisateur reste sur la même page
-}
-
-
-//local storage
-class Storage {
-    static saveProducts(products) {
-      localStorage.setItem("products", JSON.stringify(products));
-    }
-    static getProduct(id) {
-      let products = JSON.parse(localStorage.getItem("products"));
-      return products.find(product => product.id === id);
-    }
-    static saveCart(cart) {
-      localStorage.setItem("cart", JSON.stringify(cart));
-    }
-    static getCart() {
-      return localStorage.getItem("cart")
-        ? JSON.parse(localStorage.getItem("cart"))
-        : [];
-    }
-  }
 
 
