fefd2410cfcdd70f981d39cbaa06edb6860a8346
Gladston Aristoverne <gladston.aristoverne.ga@gmail.com>
2020-08-10 18:54:04 -0400

    function to show in nav the number of products in cart

--- views/js/productInCart.js
+++ views/js/productInCart.js
@@ -1,8 +1,13 @@
 const cartItems = document.querySelector('.cart-items');
 
+
 document.addEventListener('DOMContentLoaded', () => {
     let getCart = JSON.parse(localStorage.getItem('cart'));
-    let cartQuantity = getCart.length;
-    cartItems.innerHTML = cartQuantity;
-    //console.log(getCart.length);
+    if (getCart <= 0) {
+        emptyCart.innerHTML = "Votre panier est vide";
+    } else{
+        let cartQuantity = getCart.length;
+        cartItems.innerHTML = cartQuantity;
+        //console.log(getCart.length);
+    }
 });
