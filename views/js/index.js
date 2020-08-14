f8d793afa31e6611d8e3af3869089dbab3ce545e
Gladston Aristoverne <gladston.aristoverne.ga@gmail.com>
2020-08-14 10:06:31 -0400

    display all products

--- views/js/index.js
+++ views/js/index.js
@@ -1,3 +1,4 @@
+const log = console.log;
 /*-------------------------------------------------------
     Nous récuperons les produits cameras et affichons tous 
     les produits dans l'index.html
@@ -5,101 +6,56 @@
 const url = "http://localhost:3000/api/cameras";
 const productsDOM = document.querySelector('.productsJs');
 
-
+// get products (cameras) with fetch API
 const getRessource = async () => {
-    // récuperation de la ressource avec l'API fetch
     const res = await fetch(url);
     const cameras = await res.json()
     return cameras
 }
 
 class Products {
-    getCameras() {
-        getRessource()
-            .then( (cameras) => {
+    getCameras(cameras) {
+        getRessource(cameras)
+            .then((cameras) => {
                 this.displayCameras(cameras);
             })
-            .then( (cameras) => {
-                this.saveCamerasInStorage(cameras)
-            })
-            // if promise is reject log error 
+            // if promise is reject log an error. 
             .catch(error => { console.log(error) })
     }
-
+    // display all products in home page
     displayCameras(cameras) {
         cameras.forEach((camera) => {
             // nous recuperons les propriéte de notre objet que nous mettons dans notre ul
             productsDOM.innerHTML += `
-        <li class="show-product">
-            <div class="products-img">
-                <img src="${camera.imageUrl}" alt="image-${camera.name}" class="img-cameras">
-            </div>
-            <div class="show-product-infos">
-                <h2> ${camera.name}</h2>
-                <a href="produit.html?id=${camera._id} ">Personnaliser</a>
-                <p> ${camera.description}</p>
-                <h3> ${camera.price} €</h3>
-                <button onclick="addToCart()" type="button" id="${camera._id}" class="btn add-to-cart">
-                    Ajouté au panier
-                    </button>
-            </div>
-        </li>`;
+            <li class="show-product">
+                <div class="products-img">
+                    <img src="${camera.imageUrl}" alt="image-${camera.name}" class="img-cameras">
+                </div>
+                <div class="show-product-infos">
+                    <h2> ${camera.name}</h2>
+                    <p> ${camera.description}</p>
+                    <h3> ${camera.price/100} €</h3>
+                    <a href="produit.html?id=${camera._id}" class="btn add-to-cart"> Personnaliser</a>
+                </div>
+            </li>`;
+        })
+        this.saveCamerasInStorage(cameras)
+    }
+    // save all product in local storage 
+    saveCamerasInStorage(cameras) {
+        cameras.map((camera) => {
+            //log(camera)
+            localStorage.setItem("camera", JSON.stringify(camera));
         })
     }
-
-        saveCamerasInStorage(cameras){
-            cameras.map((camera) => {
-                localStorage.setItem("camera", JSON.stringify(camera));
-            })
-        }
 
 }
-
-    /*---------------------------------------------------
-                   ADD TO CART - LOCAL STORAGE
-    -----------------------------------------------------*/
-    const addToCart = () => {
-        // get all buttons 
-        const cartBtn = document.querySelectorAll('.add-to-cart');
-        let cart = {
-            'itemsCart': []
-        }
-
-        cartBtn.forEach((btn) => {
-            btn.addEventListener('click', (event) => {
-                //console.log(event.target)
-                if (event.target.parentElement.classList.contains('show-product-infos')) {
-                    let id = btn.id;
-                    //let name = ;
-                    let items = {
-                        'id': id,
-                        'name': 'name',
-                        'price': 'price',
-                        'image': 'image'
-                    }
-                    cart.itemsCart.items = items;
-                    console.log(cart);
-
-                    localStorage.setItem('itemsCart', JSON.stringify(cart.itemsCart));
-                    let getItems = JSON.parse(localStorage.getItem(cart.itemsCart));
-
-                    console.log(getItems)
-                }
-            })
-        })
-
-    }
-
-
-
-
-    // Execute when Dom loaded
-    document.addEventListener("DOMContentLoaded", () => {
-        // we create a new instance of Products Class
-        const products = new Products();
-        products.getCameras()
-
-    });
+// Execute when Dom loaded
+document.addEventListener("DOMContentLoaded", () => {
+    // we create a new instance of Products Class and execute the method getCamera
+    const products = new Products();
+    products.getCameras()
+});
 
 
 
