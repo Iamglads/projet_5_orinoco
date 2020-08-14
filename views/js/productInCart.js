6020f324e46e1fa318ecfca1403a69fc95298d84
Gladston Aristoverne <gladston.aristoverne.ga@gmail.com>
2020-08-14 10:07:52 -0400

    update item cart if cart empty or not

--- views/js/productInCart.js
+++ views/js/productInCart.js
@@ -1,13 +1,17 @@
 const cartItems = document.querySelector('.cart-items');
-
+const displayProductsIncart = document.querySelector('.products__cart--list');
+const sectionInfosCart = document.querySelector('.section__infosCart');
+const continueShopping = document.querySelector('.btn-payment');
 
 document.addEventListener('DOMContentLoaded', () => {
     let getCart = JSON.parse(localStorage.getItem('cart'));
     if (getCart <= 0) {
-        emptyCart.innerHTML = "Votre panier est vide";
+       displayProductsIncart.innerHTML = `<p class="empty-cart">Votre panier est vide</p>`;
+        sectionInfosCart.style.display = "none";
+        continueShopping.style.display = "none";
     } else{
         let cartQuantity = getCart.length;
         cartItems.innerHTML = cartQuantity;
-        //console.log(getCart.length);
+        console.log(getCart.length);
     }
 });
