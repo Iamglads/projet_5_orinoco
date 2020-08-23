284bf014722e25bcd556ba7410232263a66a0b4f
Gladston Aristoverne <gladston.aristoverne.ga@gmail.com>
2020-08-22 23:29:27 -0400

    add confirm.js

---
+++ views/js/confirm.js
@@ -0,0 +1,11 @@
+const sectionConfirm = document.querySelector('.section__confirm--bloc');
+
+const dataServer = JSON.parse(sessionStorage.getItem('dataServer'));
+
+console.log(dataServer);
+
+sectionConfirm.innerHTML = `
+    <p class="empty-cart">Bonjour ${dataServer.contact.firstName} ${dataServer.contact.lastName}, Votre commande est enregistrée.
+        Veuillez noter votre numéro de commande: <br /> <mark>${dataServer.orderId}</mark>, <br /> vous allez recevoir un email avec le résumé de la commande.</p>
+        <div class="div-check"> <i class="fas fa-check"></i></div>
+    `;
