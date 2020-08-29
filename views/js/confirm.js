2925851b71b389afb551b82d0264126b25ecf57a
Gladston Aristoverne <gladston.aristoverne.ga@gmail.com>
2020-08-28 21:54:43 -0400

    delete function calculateTotalPrice

--- views/js/confirm.js
+++ views/js/confirm.js
@@ -1,30 +1,22 @@
 const sectionConfirm = document.querySelector('.section__confirm--bloc');
 const dataServer = JSON.parse(sessionStorage.getItem('dataServer'));
+const totalPriceOrder = JSON.parse(sessionStorage.getItem('totalPriceOrder'))
 
 document.addEventListener('DOMContentLoaded', () => {
     displayResumeOrder()
 })
 
-console.log(dataServer);
-
+// display the resume of order, total price and the orderId sent by server 
 function displayResumeOrder() {
     sectionConfirm.innerHTML = `
-    <p class="empty-cart">Bonjour ${dataServer.contact.firstName} ${dataServer.contact.lastName}, Votre commande d'un montant total de <b>${totalPriceFunction()/100}€</b> est enregistrée.
+    <p class="empty-cart">Bonjour ${dataServer.contact.firstName} ${dataServer.contact.lastName}, Votre commande d'un montant total de <b>${totalPriceOrder}€</b> est enregistrée.
         Veuillez noter votre numéro de commande: <br /> <b>${dataServer.orderId}</b>, <br /> vous allez recevoir un email avec le résumé de la commande.
     </p>
     <div class="div-check"> <i class="fas fa-check"></i></div>`;
 }
 
-function totalPriceFunction() {
-    let totalPrice = 0
-    for (let i = 0; i < dataServer.products.length; i++) {
-        totalPrice += dataServer.products[i].price;
-        
-    }
-    return totalPrice
-}
-
-console.log('Montant total:' + totalPriceFunction()/100 + '€')
+console.log(dataServer);
+console.log('Montant total: ' + totalPriceOrder + '€');
 
 
 
